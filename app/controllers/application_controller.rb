434745e81d6cf3d99ba9356bc737cccbd59a0e4f
Peng Liu <pengliu12@gmail.com>
2017-12-04 19:08:02 -0500

    Redirect
    Adding post sign in redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,28 @@
+require 'json'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   protected
+
+  def after_sign_in_path_for(resource_or_scope)
+    if current_user.role == 'staff'
+      puts 'Staff'
+      return '/'
+    elsif current_user.role == 'charge_nurse'
+      puts 'Nurse'
+      return '/nurse#index'
+    elsif current_user.role == 'doctor'
+      puts 'Doctor'
+      return '/doctor#index'
+    else
+      puts 'RIP'
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    return '/'
+  end
 
   def authenticate_user!
     if user_signed_in?
